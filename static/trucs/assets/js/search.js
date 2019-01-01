e96c29df43249aede89b6a743cafcf82c23d1c39
Voxylu <hello@voxylu.xyz>
2019-01-01 22:06:28 +0100

    Add the documentation for `trucs`.

---
+++ static/trucs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"isInt","url":"modules/_index_.html#isint","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""},{"id":2,"kind":64,"name":"random","url":"modules/_index_.html#random","classes":"tsd-kind-function tsd-parent-kind-external-module tsd-has-type-parameter","parent":"\"index\""},{"id":3,"kind":64,"name":"range","url":"modules/_index_.html#range","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""}]};
